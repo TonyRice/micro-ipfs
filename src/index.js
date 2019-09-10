73fb486654e71ac86f1d81767b75c3959a0d6fc2
Tony Rice <tony@tonyrice.me>
2019-09-10 00:44:49 -0500

    adding add functionality

--- src/index.js
+++ src/index.js
@@ -10,14 +10,30 @@ const config = require('./config');
 const toStream = require('pull-stream-to-stream')
 
 IPFS.create(config).then((ipfs) => {
-
     router.post('/cat', (ctx, next) => {
         const {path} = ctx.request.body;
         ctx.request.socket.setTimeout(
             5 * 60 * 1000
         );
-        const stream = ipfs.catPullStream(path);
-        ctx.body = toStream.source(stream);
+        ctx.body = toStream.source(
+            ipfs.catPullStream(path)
+        );
+    });
+
+    router.post('/add', (ctx, next) => {
+        const {data} = ctx.request.body;
+
+        ctx.request.socket.setTimeout(
+            5 * 60 * 1000
+        );
+        return ipfs.add(new Buffer(data)).then((resp) => {
+            if (resp.length === 1) {
+                ctx.body = resp[0]["hash"];
+            } else {
+                ctx.body = resp;
+            }
+            return next();
+        });
     });
 
     router.get('/health', ctx => {
